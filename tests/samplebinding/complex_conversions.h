315c1fa52fcb7976b58bd07aa88b306fa1c4d7e7
Marcelo Lira <marcelo.lira@openbossa.org>
2009-12-21 17:33:36 -0300

    Removes unnecessary comments from Complex converter test header.

--- tests/samplebinding/complex_conversions.h
+++ tests/samplebinding/complex_conversions.h
@@ -7,15 +7,6 @@ struct Converter<Complex>
     }
     static PyObject* toPython(Complex cpx)
     {
-        /*
-        fprintf(stderr, "[%s:%d] cpx.real: %f, cpx.imag: %f\n",
-                __PRETTY_FUNCTION__, __LINE__, cpx.value.real(), cpx.value.imag());
-        PyObject* result = PyComplex_FromDoubles(cpx.value.real(), cpx.value.imag());
-        fprintf(stderr, "[%s:%d]", __PRETTY_FUNCTION__, __LINE__);
-        PyObject_Print(result, stderr, 0);
-        fprintf(stderr, "\n");
-        return result;
-        */
         return PyComplex_FromDoubles(cpx.real(), cpx.imag());
     }
     static Complex toCpp(PyObject* pyobj)
