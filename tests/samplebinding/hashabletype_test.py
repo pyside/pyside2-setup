dd70b0c75366379f89e0bb46c15638d5f54904ab
Hugo Lima <hugo.lima@openbossa.org>
2010-03-19 14:55:33 -0300

    Added a unit test to check the object-type hashability.
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
    Reviewer: Bruno Araújo <bruno.araujo@openbossa.org>

--- tests/samplebinding/hashabletype_test.py
+++ tests/samplebinding/hashabletype_test.py
@@ -32,11 +32,17 @@ from sample import *
 
 class HashableTest(unittest.TestCase):
 
-    def testAbstractPureVirtualMethodAvailability(self):
+    def testStrHash(self):
         h = {}
         s = Str("Hi")
         h[s] = 2
         self.assert_(h.get(s), 2)
+
+    def testObjectTypeHash(self):
+        h = {}
+        o = ObjectType()
+        h[o] = 2
+        self.assert_(h.get(o), 2)
 
 if __name__ == '__main__':
     unittest.main()
