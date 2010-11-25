c57ec52139c7aa878341fd0f61133d4472d5fc69
Marcelo Lira <marcelo.lira@openbossa.org>
2010-11-25 16:49:04 -0300

    Cast PySideSignal object to PyObject before calling Py_DECREF.
    This is done because the Py_DECREF macro from Python 2.5 doesn't
    cast the argument to "PyObject*" as 2.6 does. This is a problem
    for PySideSignal objects, for this struct is private, having only
    the forward declaration visible.
    
    Reviewed by Hugo Parente <hugo.lima@openbossa.org>
    Reviewed by Renato Araújo <renato.filho@openbossa.org>

--- generator/cppgenerator.cpp
+++ generator/cppgenerator.cpp
@@ -3044,7 +3044,7 @@ void CppGenerator::writeSignalInitialization(QTextStream& s, const AbstractMetaC
         s << ", NULL);" << endl;
         s << INDENT << "PySide::Signal::addSignalToWrapper(&" + cpythonTypeName(metaClass) + ", \"";
         s << funcName << "\", signal_item);" << endl;
-        s << INDENT << "Py_DECREF(signal_item);" << endl;
+        s << INDENT << "Py_DECREF((PyObject*) signal_item);" << endl;
     }
     s << endl;
 }
