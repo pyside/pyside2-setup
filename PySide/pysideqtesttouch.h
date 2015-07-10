c36ef75f3d9f95935beff18d7c53fc92c54ddbed
Christian Tismer <ctismer@gmail.com>
2015-07-10 21:42:48 +0200

    Support QtTest and the special PySideQTouchEventSequence
    This was a bit difficult to understand. After I realized, that the code is mostly a copy
    of Qt code, things became clearer: The modules corrected a bit in Qt 4.6 and also
    turned the sequence into heap-based.
    I did the same for Qt5 (the correction was no longer needed).
    I tried very hard, but still cannot find out why signature
        'generateTouchEvent(QWindow*,QTouchDevice*,bool)'
    is not found. May somebody enlighten me.

--- PySide/pysideqtesttouch.h
+++ PySide/pysideqtesttouch.h
@@ -1,39 +1,31 @@
 /****************************************************************************
 **
-** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
 ** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of the QtTest module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL$
+** $QT_BEGIN_LICENSE:LGPL21$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Digia.  For licensing terms and
-** conditions see http://qt.digia.com/licensing.  For further information
+** a written agreement between you and Digia. For licensing terms and
+** conditions see http://qt.digia.com/licensing. For further information
 ** use the contact form at http://qt.digia.com/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** General Public License version 2.1 or version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE.LGPLv21 and
+** LICENSE.LGPLv3 included in the packaging of this file. Please review the
+** following information to ensure the GNU Lesser General Public License
+** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
 ** In addition, as a special exception, Digia gives you certain additional
-** rights.  These rights are described in the Digia Qt LGPL Exception
+** rights. These rights are described in the Digia Qt LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
 **
 ** $QT_END_LICENSE$
 **
@@ -50,7 +42,10 @@
 
 #include <QtCore/qmap.h>
 #include <QtGui/qevent.h>
+#include <QtGui/qwindow.h>
+#ifdef QT_WIDGETS_LIB
 #include <QtWidgets/qwidget.h>
+#endif
 
 QT_BEGIN_NAMESPACE
 
@@ -62,9 +57,38 @@ namespace QTest
     public:
         ~PySideQTouchEventSequence()
         {
-            commit();
+            if (commitWhenDestroyed)
+                commit();
+        }
+        PySideQTouchEventSequence* press(int touchId, const QPoint &pt, QWindow *window = 0)
+        {
+            QTouchEvent::TouchPoint &p = point(touchId);
+            p.setScreenPos(mapToScreen(window, pt));
+            p.setState(Qt::TouchPointPressed);
+            return this;
+        }
+        PySideQTouchEventSequence* move(int touchId, const QPoint &pt, QWindow *window = 0)
+        {
+            QTouchEvent::TouchPoint &p = point(touchId);
+            p.setScreenPos(mapToScreen(window, pt));
+            p.setState(Qt::TouchPointMoved);
+            return this;
+        }
+        PySideQTouchEventSequence* release(int touchId, const QPoint &pt, QWindow *window = 0)
+        {
+            QTouchEvent::TouchPoint &p = point(touchId);
+            p.setScreenPos(mapToScreen(window, pt));
+            p.setState(Qt::TouchPointReleased);
+            return this;
+        }
+        PySideQTouchEventSequence* stationary(int touchId)
+        {
+            QTouchEvent::TouchPoint &p = pointOrPreviousPoint(touchId);
+            p.setState(Qt::TouchPointStationary);
+            return this;
         }
 
+#ifdef QT_WIDGETS_LIB
         PySideQTouchEventSequence* press(int touchId, const QPoint &pt, QWidget *widget = 0)
         {
             QTouchEvent::TouchPoint &p = point(touchId);
@@ -88,30 +112,43 @@ namespace QTest
             p.setState(Qt::TouchPointReleased);
             return this;
         }
+#endif
 
-        PySideQTouchEventSequence* stationary(int touchId)
+        void commit(bool processEvents = true)
         {
-            QTouchEvent::TouchPoint &p = point(touchId);
-            p.setState(Qt::TouchPointStationary);
-            return this;
-        }
-
-        void commit()
-        {
-            qt_translateRawTouchEvent(targetWidget, deviceType, points.values());
-            targetWidget = 0;
+            if (!points.isEmpty()) {
+                if (targetWindow)
+                {
+                    qt_handleTouchEvent(targetWindow, device, points.values());
+                }
+#ifdef QT_WIDGETS_LIB
+                else if (targetWidget)
+                {
+                    qt_handleTouchEvent(targetWidget->windowHandle(), device, points.values());
+                }
+#endif
+            }
+            if (processEvents)
+                QCoreApplication::processEvents();
+            previousPoints = points;
             points.clear();
         }
 
-    private:
-        PySideQTouchEventSequence(QWidget *widget, QTouchEvent::DeviceType aDeviceType)
-            : targetWidget(widget), deviceType(aDeviceType)
+private:
+#ifdef QT_WIDGETS_LIB
+        PySideQTouchEventSequence(QWidget *widget, QTouchDevice *aDevice, bool autoCommit)
+            : targetWidget(widget), targetWindow(0), device(aDevice), commitWhenDestroyed(autoCommit)
         {
         }
-
-        PySideQTouchEventSequence(const PySideQTouchEventSequence &v);
-
-        void operator=(const PySideQTouchEventSequence&);
+#endif
+        PySideQTouchEventSequence(QWindow *window, QTouchDevice *aDevice, bool autoCommit)
+            :
+#ifdef QT_WIDGETS_LIB
+              targetWidget(0),
+#endif
+              targetWindow(window), device(aDevice), commitWhenDestroyed(autoCommit)
+        {
+        }
 
         QTouchEvent::TouchPoint &point(int touchId)
         {
@@ -120,24 +157,61 @@ namespace QTest
             return points[touchId];
         }
 
+        QTouchEvent::TouchPoint &pointOrPreviousPoint(int touchId)
+        {
+            if (!points.contains(touchId)) {
+                if (previousPoints.contains(touchId))
+                    points[touchId] = previousPoints.value(touchId);
+                else
+                    points[touchId] = QTouchEvent::TouchPoint(touchId);
+            }
+            return points[touchId];
+        }
+
+#ifdef QT_WIDGETS_LIB
         QPoint mapToScreen(QWidget *widget, const QPoint &pt)
         {
             if (widget)
                 return widget->mapToGlobal(pt);
             return targetWidget ? targetWidget->mapToGlobal(pt) : pt;
         }
+#endif
+        QPoint mapToScreen(QWindow *window, const QPoint &pt)
+        {
+            if(window)
+                return window->mapToGlobal(pt);
+            return targetWindow ? targetWindow->mapToGlobal(pt) : pt;
+        }
 
+        QMap<int, QTouchEvent::TouchPoint> previousPoints;
         QMap<int, QTouchEvent::TouchPoint> points;
+#ifdef QT_WIDGETS_LIB
         QWidget *targetWidget;
-        QTouchEvent::DeviceType deviceType;
-        friend PySideQTouchEventSequence* generateTouchEvent(QWidget *, QTouchEvent::DeviceType);
+#endif
+        QWindow *targetWindow;
+        QTouchDevice *device;
+        bool commitWhenDestroyed;
+#ifdef QT_WIDGETS_LIB
+        friend PySideQTouchEventSequence* generateTouchEvent(QWidget *, QTouchDevice*, bool);
+#endif
+        friend PySideQTouchEventSequence* generateTouchEvent(QWindow *, QTouchDevice*, bool);
     };
 
+#ifdef QT_WIDGETS_LIB
     inline
-    PySideQTouchEventSequence* generateTouchEvent(QWidget *widget = 0,
-                                                  QTouchEvent::DeviceType deviceType = QTouchEvent::TouchScreen)
+    PySideQTouchEventSequence* generateTouchEvent(QWidget *widget,
+                                                  QTouchDevice *device,
+                                                  bool autoCommit = true)
     {
-        return new PySideQTouchEventSequence(widget, deviceType);
+        return new PySideQTouchEventSequence(widget, device, autoCommit);
+    }
+#endif
+    inline
+    PySideQTouchEventSequence* generateTouchEvent(QWindow *window,
+                                                  QTouchDevice *device,
+                                                  bool autoCommit = true)
+    {
+        return new PySideQTouchEventSequence(window, device, autoCommit);
     }
 
 }
