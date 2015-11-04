4a91bf1fa5a7966e5d3f9ad61e3c164625345e48
jumaroch <juma@roch.com>
2015-11-04 18:32:29 +0100

    remove metaObject method (done when parsing sources with Q_DOC macro)

--- generator/shiboken/headergenerator.cpp
+++ generator/shiboken/headergenerator.cpp
@@ -133,7 +133,6 @@ void HeaderGenerator::generateClass(QTextStream& s, const AbstractMetaClass* met
         if ((!avoidProtectedHack() || !metaClass->hasPrivateDestructor())
             && usePySideExtensions() && metaClass->isQObject()) {
             s << "public:\n";
-            s << INDENT << "const QMetaObject* metaObject() const;" << endl;
             s << INDENT << "virtual int qt_metacall(QMetaObject::Call call, int id, void** args);" << endl;
             s << INDENT << "virtual void* qt_metacast(const char* _clname);" << endl;
         }
