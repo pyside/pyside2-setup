1617aca75864d400de8324bd74131bf15871510a
Christian Tismer <ctismer@gmail.com>
2015-11-07 15:54:02 +0100

    allow building for python 3.5, although that does not work on Windows, yet

--- setup.py
+++ setup.py
@@ -1031,6 +1031,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Database',
         'Topic :: Software Development',
         'Topic :: Software Development :: Code Generators',
