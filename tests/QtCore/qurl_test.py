1d709a3a0dafd97bee4a7d861dd7db8c708ab160
Friedemann Kleint <Friedemann.Kleint@qt.io>
2016-08-24 12:42:22 +0200

    qurl_test.py: Comment out tests for query items
    These tests use a new API in Qt 5.
    
    Task-number: PYSIDE-345
    Change-Id: I70ee42f3375893a2caf07a5525e3d5aa828a7144
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- tests/QtCore/qurl_test.py
+++ tests/QtCore/qurl_test.py
@@ -38,53 +38,54 @@ class QUrlBasicConstructor(unittest.TestCase):
         self.assertEqual(url.toString(),
                         'ftp://john:abc123@www.google.com:8080/mail/view')
 
-class QueryItemsTest(unittest.TestCase):
-    '''Test query item management'''
-
-    def testQueryItems(self):
-        #QUrl.queryItems
-        url = QUrl('http://www.google.com/search?q=python&hl=en')
-        valid_data = [(('q'), ('python')), (('hl'), ('en'))]
-
-        self.assertEqual(sorted(url.queryItems()), sorted(valid_data))
-
-    def testEncodedQueryItems(self):
-        #QUrl.encodedQueryItems
-        url = QUrl('http://www.google.com/search?q=python&hl=en')
-        valid_data = [(('q'), ('python')), (('hl'), ('en'))]
-
-        self.assertEqual(sorted(url.encodedQueryItems()), sorted(valid_data))
-
-    def testSetQueryItems(self):
-        #QUrl.setQueryItems
-        urla = QUrl('http://www.google.com/search?q=python&hl=en')
-        urlb = QUrl('http://www.google.com/search')
-
-        urlb.setQueryItems(urla.queryItems())
-
-        self.assertEqual(urla, urlb)
-
-    def testAddQueryItem(self):
-        #QUrl.addQueryItem
-        url = QUrl()
-        valid_data = [('hl', 'en'), ('user', 'konqui')]
-
-        url.addQueryItem(*valid_data[0])
-        self.assertEqual(url.queryItems()[0], valid_data[0])
-
-        url.addQueryItem(*valid_data[1])
-        self.assertEqual(sorted(url.queryItems()), sorted(valid_data))
-
-    def testAllEncodedQueryItemsValues(self):
-        #QUrl.allEncodedQueryItemValues
-        url = QUrl()
-        key = 'key'
-        valid_data = ['data', 'valid', 'test']
-
-        for i, data in enumerate(valid_data):
-            url.addQueryItem(key, data)
-            self.assertEqual(url.allEncodedQueryItemValues(key),
-                             list(valid_data[:i+1]))
+# PYSIDE-345: No bindings for QUrlQuery
+# class QueryItemsTest(unittest.TestCase):
+#     '''Test query item management'''
+#
+#     def testQueryItems(self):
+#         #QUrl.queryItems
+#         url = QUrl('http://www.google.com/search?q=python&hl=en')
+#         valid_data = [(('q'), ('python')), (('hl'), ('en'))]
+#
+#         self.assertEqual(sorted(url.queryItems()), sorted(valid_data))
+#
+# def testEncodedQueryItems(self):
+#         #QUrl.encodedQueryItems
+#         url = QUrl('http://www.google.com/search?q=python&hl=en')
+#         valid_data = [(('q'), ('python')), (('hl'), ('en'))]
+#
+#         self.assertEqual(sorted(url.encodedQueryItems()), sorted(valid_data))
+#
+#     def testSetQueryItems(self):
+#         #QUrl.setQueryItems
+#         urla = QUrl('http://www.google.com/search?q=python&hl=en')
+#         urlb = QUrl('http://www.google.com/search')
+#
+#         urlb.setQueryItems(urla.queryItems())
+#
+#         self.assertEqual(urla, urlb)
+#
+#     def testAddQueryItem(self):
+#         #QUrl.addQueryItem
+#         url = QUrl()
+#         valid_data = [('hl', 'en'), ('user', 'konqui')]
+#
+#         url.addQueryItem(*valid_data[0])
+#         self.assertEqual(url.queryItems()[0], valid_data[0])
+#
+#         url.addQueryItem(*valid_data[1])
+#         self.assertEqual(sorted(url.queryItems()), sorted(valid_data))
+#
+#     def testAllEncodedQueryItemsValues(self):
+#         #QUrl.allEncodedQueryItemValues
+#         url = QUrl()
+#         key = 'key'
+#         valid_data = ['data', 'valid', 'test']
+#
+#         for i, data in enumerate(valid_data):
+#             url.addQueryItem(key, data)
+#             self.assertEqual(url.allEncodedQueryItemValues(key),
+#                              list(valid_data[:i+1]))
 
 
 if __name__ == '__main__':
