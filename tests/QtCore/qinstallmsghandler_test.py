616aea84ef5c713764d68f1c6839018a1c4a0570
Friedemann Kleint <Friedemann.Kleint@qt.io>
2016-08-25 12:48:10 +0200

    Port installmsghandler_test.py
    Port it to using the new API in Qt 5
    
    Task-number: PYSIDE-346
    Change-Id: I29d93baeb949e809745dcc78916d36a58e3709ee
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- tests/QtCore/qinstallmsghandler_test.py
+++ tests/QtCore/qinstallmsghandler_test.py
@@ -8,45 +8,45 @@ from PySide2.QtCore import *
 
 param = []
 
-def handler(msgt, msg):
+def handler(msgt, ctx, msg):
     global param
-    param = [msgt, msg.strip()]
+    param = [msgt, ctx, msg.strip()]
 
-def handleruseless(msgt, msg):
+def handleruseless(msgt, ctx, msg):
     pass
 
 class QInstallMsgHandlerTest(unittest.TestCase):
 
     def tearDown(self):
         # Ensure that next test will have a clear environment
-        qInstallMsgHandler(None)
+        qInstallMessageHandler(None)
 
     def testNone(self):
-        ret = qInstallMsgHandler(None)
+        ret = qInstallMessageHandler(None)
         self.assertEqual(ret, None)
 
     def testRet(self):
-        ret = qInstallMsgHandler(None)
+        ret = qInstallMessageHandler(None)
         self.assertEqual(ret, None)
         refcount = sys.getrefcount(handleruseless)
-        retNone = qInstallMsgHandler(handleruseless)
+        retNone = qInstallMessageHandler(handleruseless)
         self.assertEqual(sys.getrefcount(handleruseless), refcount + 1)
-        rethandler = qInstallMsgHandler(None)
+        rethandler = qInstallMessageHandler(None)
         self.assertEqual(rethandler, handleruseless)
         del rethandler
         self.assertEqual(sys.getrefcount(handleruseless), refcount)
 
     def testHandler(self):
-        rethandler = qInstallMsgHandler(handler)
+        rethandler = qInstallMessageHandler(handler)
         qDebug("Test Debug")
         self.assertEqual(param[0], QtDebugMsg)
-        self.assertEqual(param[1], "Test Debug")
+        self.assertEqual(param[2], "Test Debug")
         qWarning("Test Warning")
         self.assertEqual(param[0], QtWarningMsg)
-        self.assertEqual(param[1], "Test Warning")
+        self.assertEqual(param[2], "Test Warning")
         qCritical("Test Critical")
         self.assertEqual(param[0], QtCriticalMsg)
-        self.assertEqual(param[1], "Test Critical")
+        self.assertEqual(param[2], "Test Critical")
 
 if __name__ == '__main__':
     unittest.main()
