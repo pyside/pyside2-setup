fcdf6ad919e14aca48199099a95cd9de2e6c21df
Friedemann Kleint <Friedemann.Kleint@qt.io>
2016-09-21 14:27:32 +0200

    qfile_test.py: Fix Python 3
    - Make the check in testBug721 depending on Python version.
    - Ensure cleanup of the temporary file in case of test failure,
      fixing windows warning:
      File "tests/QtCore/qfile_test.py", line 21, in tearDown
        os.remove(self.filename)
    PermissionError: [WinError 32] The process cannot access the file because it is being used by another process: 'd:\\temp\\tmp27nmak6m'
    
    Change-Id: Ie69de51658109f5a9e649f9377b686a6a291cfe0
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- tests/QtCore/qfile_test.py
+++ tests/QtCore/qfile_test.py
@@ -3,6 +3,7 @@ import unittest
 
 import os
 import tempfile
+import sys
 import py3kcompat as py3k
 
 from PySide2.QtCore import QFile, QIODevice
@@ -24,20 +25,28 @@ class GetCharTest(unittest.TestCase):
         '''QFile.getChar'''
         obj = QFile(self.filename)
         obj.open(QIODevice.ReadOnly)
-        self.assertEqual(obj.getChar(), (True, 'a'))
-        self.assertFalse(obj.getChar()[0])
-        obj.close()
+        try:
+            self.assertEqual(obj.getChar(), (True, 'a'))
+            self.assertFalse(obj.getChar()[0])
+        finally:
+            obj.close()
 
     def testBug721(self):
         obj = QFile(self.filename)
         obj.open(QIODevice.ReadOnly)
-        memory = obj.map(0, 1)
-        self.assertEqual(len(memory), 1)
-        self.assertEqual(memory[0], py3k.b('a'))
-        obj.unmap(memory)
-        # now memory points to wild bytes... :-)
-        # uncommenting this must cause a segfault.
-        # self.assertEqual(memory[0], 'a')
+        try:
+            memory = obj.map(0, 1)
+            self.assertEqual(len(memory), 1)
+            if sys.version_info[0] >= 3:
+                self.assertEqual(memory[0], ord('a'))
+            else:
+                self.assertEqual(memory[0], py3k.b('a'))
+            # now memory points to wild bytes... :-)
+            # uncommenting this must cause a segfault.
+            # self.assertEqual(memory[0], 'a')
+        finally:
+            obj.unmap(memory)
+            obj.close()
 
 if __name__ == '__main__':
     unittest.main()
