5be78207ef21c8fe6170aa4e711ce52ef0dca485
Alexandru Croitor <alexandru.croitor@qt.io>
2018-05-25 11:10:09 +0200

    Remove Python 2.6 package classifier
    Change-Id: I200b87bc4e66d17d8eefe42359ab6bcc36d24f6c
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- setup.py
+++ setup.py
@@ -249,7 +249,6 @@ setup(
         'Programming Language :: C++',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
