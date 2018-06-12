c203fece799528502e61fd8435a93fb751be318b
Alexandru Croitor <alexandru.croitor@qt.io>
2018-06-12 09:34:04 +0200

    Bump 5.11 branch version to 5.11.1a1
    This is to start generating snapshot wheels again, so that they would
    not override the v5.11.0 release wheels.
    
    Change-Id: I1b4c5211471ec760cad14d56aa3ee2102fea5322
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    Reviewed-by: Simo Fält <simo.falt@qt.io>

--- sources/shiboken2/shiboken_version.py
+++ sources/shiboken2/shiboken_version.py
@@ -39,15 +39,15 @@
 
 major_version = "5"
 minor_version = "11"
-patch_version = "0"
+patch_version = "1"
 
 # For example: "a", "b", "rc"
 # (which means "alpha", "beta", "release candidate").
 # An empty string means the generated package will be an official release.
-pre_release_version_type = ""
+pre_release_version_type = "a"
 
 # For example: "1", "2" (which means "beta1", "beta2", if type is "b").
-pre_release_version = ""
+pre_release_version = "1"
 
 if __name__ == '__main__':
     # Used by CMake.
