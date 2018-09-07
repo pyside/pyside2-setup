824b7733c0bd8b162b198c67014d7f008fb71b8c
Simo Fält <simo.falt@qt.io>
2018-09-07 09:33:52 +0300

    Cleanup version strings for 5.11.2 release
    Change-Id: I73873b8e98edfc8b9544b646fb7d11c6cfe551cf
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- sources/pyside2/pyside_version.py
+++ sources/pyside2/pyside_version.py
@@ -39,15 +39,15 @@
 
 major_version = "5"
 minor_version = "11"
-patch_version = "1"
+patch_version = "2"
 
 # For example: "a", "b", "rc"
 # (which means "alpha", "beta", "release candidate").
 # An empty string means the generated package will be an official release.
-pre_release_version_type = "a"
+pre_release_version_type = ""
 
 # For example: "1", "2" (which means "beta1", "beta2", if type is "b").
-pre_release_version = "1"
+pre_release_version = ""
 
 if __name__ == '__main__':
     # Used by CMake.
