6d5ca98cad443cc3d10e28262d9a05ff47ba660a
Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>
2019-04-29 11:32:51 +0200

    Example: Port the QtSql Books example
    Change-Id: Ie8b0378f6f8de406700cf8ace381e0022b641617
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

---
+++ examples/sql/books/bookdelegate.py
@@ -0,0 +1,134 @@
+#############################################################################
+##
+## Copyright (C) 2019 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the Qt for Python examples of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:BSD$
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##   * Neither the name of The Qt Company Ltd nor the names of its
+##     contributors may be used to endorse or promote products derived
+##     from this software without specific prior written permission.
+##
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+import copy
+from PySide2.QtSql import QSqlRelationalDelegate
+from PySide2.QtWidgets import (QItemDelegate, QSpinBox, QStyledItemDelegate,
+    QStyle, QStyleOptionViewItem)
+from PySide2.QtGui import QMouseEvent, QPixmap, QPalette
+from PySide2.QtCore import QEvent, QSize, Qt
+
+
+class BookDelegate(QSqlRelationalDelegate):
+    """Books delegate to rate the books"""
+
+    def __init__(self, parent=None):
+        QSqlRelationalDelegate.__init__(self, parent)
+        self.star = QPixmap(":/images/star.png")
+
+    def paint(self, painter, option, index):
+        """ Paint the items in the table.
+
+            If the item referred to by <index> is a StarRating, we
+            handle the painting ourselves. For the other items, we
+            let the base class handle the painting as usual.
+
+            In a polished application, we'd use a better check than
+            the column number to find out if we needed to paint the
+            stars, but it works for the purposes of this example.
+        """
+        if index.column() != 5:
+            # Since we draw the grid ourselves:
+            opt = copy.copy(option)
+            opt.rect = option.rect.adjusted(0, 0, -1, -1)
+            QSqlRelationalDelegate.paint(self, painter, opt, index)
+        else:
+            model = index.model()
+            if option.state & QStyle.State_Enabled:
+                if option.state & QStyle.State_Active:
+                    color_group = QPalette.Normal
+                else:
+                    color_group = QPalette.Inactive
+            else:
+                color_group = QPalette.Disabled
+
+            if option.state & QStyle.State_Selected:
+                painter.fillRect(option.rect,
+                    option.palette.color(color_group, QPalette.Highlight))
+            rating = model.data(index, Qt.DisplayRole)
+            width = self.star.width()
+            height = self.star.height()
+            x = option.rect.x()
+            y = option.rect.y() + (option.rect.height() / 2) - (height / 2)
+            for i in range(rating):
+                painter.drawPixmap(x, y, self.star)
+                x += width
+
+            # Since we draw the grid ourselves:
+            self.drawFocus(painter, option, option.rect.adjusted(0, 0, -1, -1))
+
+        pen = painter.pen()
+        painter.setPen(option.palette.color(QPalette.Mid))
+        painter.drawLine(option.rect.bottomLeft(), option.rect.bottomRight())
+        painter.drawLine(option.rect.topRight(), option.rect.bottomRight())
+        painter.setPen(pen)
+
+    def sizeHint(self, option, index):
+        """ Returns the size needed to display the item in a QSize object. """
+        if index.column() == 5:
+            size_hint = QSize(5 * self.star.width(), self.star.height()) + QSize(1, 1)
+            return size_hint
+        # Since we draw the grid ourselves:
+        return QSqlRelationalDelegate.sizeHint(self, option, index) + QSize(1, 1)
+
+    def editorEvent(self, event, model, option, index):
+        if index.column() != 5:
+            return False
+
+        if event.type() == QEvent.MouseButtonPress:
+            mouse_pos = event.pos()
+            new_stars = int(0.7 + (mouse_pos.x() - option.rect.x()) / self.star.width())
+            stars = max(0, min(new_stars, 5))
+            model.setData(index, stars)
+            # So that the selection can change
+            return False
+
+        return True
+
+    def createEditor(self, parent, option, index):
+        if index.column() != 4:
+            return QSqlRelationalDelegate.createEditor(self, parent, option, index)
+
+        # For editing the year, return a spinbox with a range from -1000 to 2100.
+        spinbox = QSpinBox(parent)
+        spinbox.setFrame(False)
+        spinbox.setMaximum(2100)
+        spinbox.setMinimum(-1000)
+        return spinbox
---
+++ examples/sql/books/ui_bookwindow.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'bookwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+class Ui_BookWindow(object):
+    def setupUi(self, BookWindow):
+        if BookWindow.objectName():
+            BookWindow.setObjectName(u"BookWindow")
+        BookWindow.resize(601, 420)
+        self.centralWidget = QWidget(BookWindow)
+        self.centralWidget.setObjectName(u"centralWidget")
+        self.vboxLayout = QVBoxLayout(self.centralWidget);
+        self.vboxLayout.setSpacing(6)
+        self.vboxLayout.setObjectName(u"vboxLayout")
+        self.vboxLayout.setContentsMargins(9, 9, 9, 9);
+        self.groupBox = QGroupBox(self.centralWidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.vboxLayout1 = QVBoxLayout(self.groupBox);
+        self.vboxLayout1.setSpacing(6)
+        self.vboxLayout1.setObjectName(u"vboxLayout1")
+        self.vboxLayout1.setContentsMargins(9, 9, 9, 9);
+        self.bookTable = QTableView(self.groupBox)
+        self.bookTable.setObjectName(u"bookTable")
+        self.bookTable.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        self.vboxLayout1.addWidget(self.bookTable);
+
+        self.groupBox_2 = QGroupBox(self.groupBox)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.formLayout = QFormLayout(self.groupBox_2);
+        self.formLayout.setObjectName(u"formLayout")
+        self.label_5 = QLabel(self.groupBox_2)
+        self.label_5.setObjectName(u"label_5")
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label_5);
+
+        self.titleEdit = QLineEdit(self.groupBox_2)
+        self.titleEdit.setObjectName(u"titleEdit")
+        self.titleEdit.setEnabled(True)
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.titleEdit);
+
+        self.label_2 = QLabel(self.groupBox_2)
+        self.label_2.setObjectName(u"label_2")
+
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2);
+
+        self.authorEdit = QComboBox(self.groupBox_2)
+        self.authorEdit.setObjectName(u"authorEdit")
+        self.authorEdit.setEnabled(True)
+
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.authorEdit);
+
+        self.label_3 = QLabel(self.groupBox_2)
+        self.label_3.setObjectName(u"label_3")
+
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_3);
+
+        self.genreEdit = QComboBox(self.groupBox_2)
+        self.genreEdit.setObjectName(u"genreEdit")
+        self.genreEdit.setEnabled(True)
+
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.genreEdit);
+
+        self.label_4 = QLabel(self.groupBox_2)
+        self.label_4.setObjectName(u"label_4")
+
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.label_4);
+
+        self.yearEdit = QSpinBox(self.groupBox_2)
+        self.yearEdit.setObjectName(u"yearEdit")
+        self.yearEdit.setEnabled(True)
+        self.yearEdit.setMinimum(-1000)
+        self.yearEdit.setMaximum(2100)
+
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.yearEdit);
+
+        self.label = QLabel(self.groupBox_2)
+        self.label.setObjectName(u"label")
+
+        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.label);
+
+        self.ratingEdit = QSpinBox(self.groupBox_2)
+        self.ratingEdit.setObjectName(u"ratingEdit")
+        self.ratingEdit.setMaximum(5)
+
+        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.ratingEdit);
+
+
+        self.vboxLayout1.addWidget(self.groupBox_2);
+
+
+        self.vboxLayout.addWidget(self.groupBox);
+
+        BookWindow.setCentralWidget(self.centralWidget)
+        QWidget.setTabOrder(self.bookTable, self.titleEdit)
+        QWidget.setTabOrder(self.titleEdit, self.authorEdit)
+        QWidget.setTabOrder(self.authorEdit, self.genreEdit)
+        QWidget.setTabOrder(self.genreEdit, self.yearEdit)
+
+        self.retranslateUi(BookWindow)
+
+        QMetaObject.connectSlotsByName(BookWindow)
+    # setupUi
+
+    def retranslateUi(self, BookWindow):
+        BookWindow.setWindowTitle(QCoreApplication.translate("BookWindow", u"Books", None))
+        self.groupBox.setTitle("")
+        self.groupBox_2.setTitle(QCoreApplication.translate("BookWindow", u"Details", None))
+        self.label_5.setText(QCoreApplication.translate("BookWindow", u"<b>Title:</b>", None))
+        self.label_2.setText(QCoreApplication.translate("BookWindow", u"<b>Author: </b>", None))
+        self.label_3.setText(QCoreApplication.translate("BookWindow", u"<b>Genre:</b>", None))
+        self.label_4.setText(QCoreApplication.translate("BookWindow", u"<b>Year:</b>", None))
+        self.yearEdit.setPrefix("")
+        self.label.setText(QCoreApplication.translate("BookWindow", u"<b>Rating:</b>", None))
+    # retranslateUi
