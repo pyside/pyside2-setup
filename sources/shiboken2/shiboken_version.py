fef1bfb9069afb64761cdac7bc219b3a510fec19
Simo Fält <simo.falt@qt.io>
2019-04-29 08:45:09 +0300

    Cleanup version strings for 5.12.3 release
    Change-Id: I5c8c79ae3680ed3a2dd2ce5c6e90916624931d7d
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- sources/shiboken2/shiboken_version.py
+++ sources/shiboken2/shiboken_version.py
@@ -39,15 +39,15 @@
 
 major_version = "5"
 minor_version = "12"
-patch_version = "2"
+patch_version = "3"
 
 # For example: "a", "b", "rc"
 # (which means "alpha", "beta", "release candidate").
 # An empty string means the generated package will be an official release.
-pre_release_version_type = "a"
+pre_release_version_type = ""
 
 # For example: "1", "2" (which means "beta1", "beta2", if type is "b").
-pre_release_version = "1"
+pre_release_version = ""
 
 if __name__ == '__main__':
     # Used by CMake.
