pyside/pyside2-setup

commit 90c1c767095e583d0315e87c0592597020858246

Pythonize tabbedbrowser example
Change the style of the code to follow PEP8

Change-Id: I93eb0df893e8305b4e65ab5318383433b0655625
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

examples/webenginewidgets/tabbedbrowser/webengineview.py
@@ -43,7 +43,7 @@ from PySide2.QtWebEngineWidgets import QWebEnginePage, QWebEngineView
 
 from PySide2 import QtCore
 
-_webActions = [QWebEnginePage.Back, QWebEnginePage.Forward,
+_web_actions = [QWebEnginePage.Back, QWebEnginePage.Forward,
                QWebEnginePage.Reload,
                QWebEnginePage.Undo, QWebEnginePage.Redo,
                QWebEnginePage.Cut, QWebEnginePage.Copy,
@@ -51,40 +51,40 @@ _webActions = [QWebEnginePage.Back, QWebEnginePage.Forward,
 
 class WebEngineView(QWebEngineView):
 
-    enabledChanged = QtCore.Signal(QWebEnginePage.WebAction, bool)
+    enabled_changed = QtCore.Signal(QWebEnginePage.WebAction, bool)
 
     @staticmethod
-    def webActions():
-        return _webActions
+    def web_actions():
+        return _web_actions
 
     @staticmethod
-    def minimumZoomFactor():
+    def minimum_zoom_factor():
         return 0.25
 
     @staticmethod
-    def maximumZoomFactor():
+    def maximum_zoom_factor():
         return 5
 
-    def __init__(self, tabFactoryFunc, windowFactoryFunc):
+    def __init__(self, tab_factory_func, window_factory_func):
         super(WebEngineView, self).__init__()
-        self._tabFactoryFunc = tabFactoryFunc
-        self._windowFactoryFunc = windowFactoryFunc
+        self._tab_factory_func = tab_factory_func
+        self._window_factory_func = window_factory_func
         page = self.page()
         self._actions = {}
-        for webAction in WebEngineView.webActions():
-            action = page.action(webAction)
-            action.changed.connect(self._enabledChanged)
-            self._actions[action] = webAction
+        for web_action in WebEngineView.web_actions():
+            action = page.action(web_action)
+            action.changed.connect(self._enabled_changed)
+            self._actions[action] = web_action
 
-    def isWebActionEnabled(self, webAction):
-        return self.page().action(webAction).isEnabled()
+    def is_web_action_enabled(self, web_action):
+        return self.page().action(web_action).isEnabled()
 
-    def createWindow(self, windowType):
-        if windowType == QWebEnginePage.WebBrowserTab or windowType == QWebEnginePage.WebBrowserBackgroundTab:
-            return self._tabFactoryFunc()
-        return self._windowFactoryFunc()
+    def create_window(self, window_type):
+        if window_type == QWebEnginePage.WebBrowserTab or window_type == QWebEnginePage.WebBrowserBackgroundTab:
+            return self._tab_factory_func()
+        return self._window_factory_func()
 
-    def _enabledChanged(self):
+    def _enabled_changed(self):
         action = self.sender()
-        webAction = self._actions[action]
-        self.enabledChanged.emit(webAction, action.isEnabled())
+        web_action = self._actions[action]
+        self.enabled_changed.emit(web_action, action.isEnabled())

examples/webenginewidgets/tabbedbrowser/bookmarkwidget.py
@@ -47,11 +47,11 @@ from PySide2.QtGui import QIcon, QPixmap, QStandardItem, QStandardItemModel
 from PySide2.QtWidgets import (QAction, QDockWidget, QMenu, QMessageBox,
     QToolBar, QTreeView, QWidget)
 
-_urlRole = Qt.UserRole + 1
+_url_role = Qt.UserRole + 1
 
 # Default bookmarks as an array of arrays which is the form
 # used to read from/write to a .json bookmarks file
-_defaultBookMarks = [
+_default_bookmarks = [
     ['Tool Bar'],
     ['http://qt.io', 'Qt', ':/qt-project.org/qmessagebox/images/qtlogo-64.png'],
     ['https://download.qt.io/snapshots/ci/pyside/', 'Downloads'],
@@ -62,60 +62,60 @@ _defaultBookMarks = [
     ['Other Bookmarks']
 ]
 
-def _configDir():
+def _config_dir():
     return '{}/QtForPythonBrowser'.format(
         QStandardPaths.writableLocation(QStandardPaths.ConfigLocation))
 
-_bookmarkFile = 'bookmarks.json'
+_bookmark_file = 'bookmarks.json'
 
-def _createFolderItem(title):
+def _create_folder_item(title):
     result = QStandardItem(title)
     result.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
     return result
 
-def _createItem(url, title, icon):
+def _create_item(url, title, icon):
     result = QStandardItem(title)
     result.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-    result.setData(url, _urlRole)
+    result.setData(url, _url_role)
     if icon is not None:
         result.setIcon(icon)
     return result
 
 # Create the model from an array of arrays
-def _createModel(parent, serializedBookmarks):
+def _create_model(parent, serialized_bookmarks):
     result = QStandardItemModel(0, 1, parent)
-    lastFolderItem = None
-    for entry in serializedBookmarks:
+    last_folder_item = None
+    for entry in serialized_bookmarks:
         if len(entry) == 1:
-            lastFolderItem = _createFolderItem(entry[0])
-            result.appendRow(lastFolderItem)
+            last_folder_item = _create_folder_item(entry[0])
+            result.appendRow(last_folder_item)
         else:
             url = QUrl.fromUserInput(entry[0])
             title = entry[1]
             icon = QIcon(entry[2]) if len(entry) > 2 and entry[2] else None
-            lastFolderItem.appendRow(_createItem(url, title, icon))
+            last_folder_item.appendRow(_create_item(url, title, icon))
     return result
 
 # Serialize model into an array of arrays, writing out the icons
 # into .png files under directory in the process
-def _serializeModel(model, directory):
+def _serialize_model(model, directory):
     result = []
-    folderCount = model.rowCount()
-    for f in range(0, folderCount):
-        folderItem = model.item(f)
-        result.append([folderItem.text()])
-        itemCount = folderItem.rowCount()
-        for i in range(0, itemCount):
-            item = folderItem.child(i)
-            entry = [item.data(_urlRole).toString(), item.text()]
+    folder_count = model.rowCount()
+    for f in range(0, folder_count):
+        folder_item = model.item(f)
+        result.append([folder_item.text()])
+        item_count = folder_item.rowCount()
+        for i in range(0, item_count):
+            item = folder_item.child(i)
+            entry = [item.data(_url_role).toString(), item.text()]
             icon = item.icon()
             if not icon.isNull():
-                iconSizes = icon.availableSizes()
-                largestSize = iconSizes[len(iconSizes) - 1]
-                iconFileName = '{}/icon{:02}_{:02}_{}.png'.format(directory,
-                               f, i, largestSize.width())
-                icon.pixmap(largestSize).save(iconFileName, 'PNG')
-                entry.append(iconFileName)
+                icon_sizes = icon.availableSizes()
+                largest_size = icon_sizes[len(icon_sizes) - 1]
+                icon_file_name = '{}/icon{:02}_{:02}_{}.png'.format(directory,
+                               f, i, largest_size.width())
+                icon.pixmap(largest_size).save(icon_file_name, 'PNG')
+                entry.append(icon_file_name)
             result.append(entry)
     return result
 
@@ -123,8 +123,8 @@ def _serializeModel(model, directory):
 # functionality to persist and populate tool bars and menus.
 class BookmarkWidget(QTreeView):
 
-    openBookmark = QtCore.Signal(QUrl)
-    openBookmarkInNewTab = QtCore.Signal(QUrl)
+    open_bookmark = QtCore.Signal(QUrl)
+    open_bookmark_in_new_tab = QtCore.Signal(QUrl)
     changed = QtCore.Signal()
 
     def __init__(self):
@@ -132,7 +132,7 @@ class BookmarkWidget(QTreeView):
         self.setRootIsDecorated(False)
         self.setUniformRowHeights(True)
         self.setHeaderHidden(True)
-        self._model = _createModel(self, self._readBookmarks())
+        self._model = _create_model(self, self._read_bookmarks())
         self.setModel(self._model)
         self.expandAll()
         self.activated.connect(self._activated)
@@ -147,120 +147,120 @@ class BookmarkWidget(QTreeView):
 
     def _activated(self, index):
         item = self._model.itemFromIndex(index)
-        self.openBookmark.emit(item.data(_urlRole))
+        self.open_bookmark.emit(item.data(_url_role))
 
-    def _actionActivated(self, index):
+    def _action_activated(self, index):
         action = self.sender()
-        self.openBookmark.emit(action.data())
+        self.open_bookmark.emit(action.data())
 
-    def _toolBarItem(self):
+    def _tool_bar_item(self):
         return self._model.item(0, 0)
 
-    def _otherItem(self):
+    def _other_item(self):
         return self._model.item(1, 0)
 
-    def addBookmark(self, url, title, icon):
-        self._otherItem().appendRow(_createItem(url, title, icon))
+    def add_bookmark(self, url, title, icon):
+        self._other_item().appendRow(_create_item(url, title, icon))
 
-    def addToolBarBookmark(self, url, title, icon):
-        self._toolBarItem().appendRow(_createItem(url, title, icon))
+    def add_tool_bar_bookmark(self, url, title, icon):
+        self._tool_bar_item().appendRow(_create_item(url, title, icon))
 
-    # Synchronize the bookmarks under parentItem to a targetObject
+    # Synchronize the bookmarks under parent_item to a target_object
     # like QMenu/QToolBar, which has a list of actions. Update
     # the existing actions, append new ones if needed or hide
     # superfluous ones
-    def _populateActions(self, parentItem, targetObject, firstAction):
-        existingActions = targetObject.actions()
-        existingActionCount = len(existingActions)
-        a = firstAction
-        rowCount = parentItem.rowCount()
-        for r in range(0, rowCount):
-            item = parentItem.child(r)
+    def _populate_actions(self, parent_item, target_object, first_action):
+        existing_actions = target_object.actions()
+        existing_action_count = len(existing_actions)
+        a = first_action
+        row_count = parent_item.rowCount()
+        for r in range(0, row_count):
+            item = parent_item.child(r)
             title = item.text()
             icon = item.icon()
-            url = item.data(_urlRole)
-            if a < existingActionCount:
-                action = existingActions[a]
+            url = item.data(_url_role)
+            if a < existing_action_count:
+                action = existing_actions[a]
                 if (title != action.toolTip()):
-                    action.setText(BookmarkWidget.shortTitle(title))
+                    action.setText(BookmarkWidget.short_title(title))
                     action.setIcon(icon)
                     action.setToolTip(title)
                     action.setData(url)
                     action.setVisible(True)
             else:
-                action = targetObject.addAction(icon, BookmarkWidget.shortTitle(title))
+                action = target_object.addAction(icon, BookmarkWidget.short_title(title))
                 action.setToolTip(title)
                 action.setData(url)
-                action.triggered.connect(self._actionActivated)
+                action.triggered.connect(self._action_activated)
             a = a + 1
-        while a < existingActionCount:
-            existingActions[a].setVisible(False)
+        while a < existing_action_count:
+            existing_actions[a].setVisible(False)
             a = a + 1
 
-    def populateToolBar(self, toolBar):
-        self._populateActions(self._toolBarItem(), toolBar, 0)
+    def populate_tool_bar(self, tool_bar):
+        self._populate_actions(self._tool_bar_item(), tool_bar, 0)
 
-    def populateOther(self, menu, firstAction):
-        self._populateActions(self._otherItem(), menu, firstAction)
+    def populate_other(self, menu, first_action):
+        self._populate_actions(self._other_item(), menu, first_action)
 
-    def _currentItem(self):
+    def _current_item(self):
         index = self.currentIndex()
         if index.isValid():
             item = self._model.itemFromIndex(index)
-            if item.parent(): # Exclude top level items
+            if item.parent(): # exclude top level items
                 return item
         return None
 
-    def contextMenuEvent(self, event):
-        contextMenu = QMenu()
-        openInNewTabAction = contextMenu.addAction("Open in New Tab")
-        removeAction = contextMenu.addAction("Remove...")
-        currentItem = self._currentItem()
-        openInNewTabAction.setEnabled(currentItem is not None)
-        removeAction.setEnabled(currentItem is not None)
-        chosenAction = contextMenu.exec_(event.globalPos())
-        if chosenAction == openInNewTabAction:
-            self.openBookmarkInNewTab.emit(currentItem.data(_urlRole))
-        elif chosenAction == removeAction:
-            self._removeItem(currentItem)
+    def context_menu_event(self, event):
+        context_menu = QMenu()
+        open_in_new_tab_action = context_menu.addAction("Open in New Tab")
+        remove_action = context_menu.addAction("Remove...")
+        current_item = self._current_item()
+        open_in_new_tab_action.setEnabled(current_item is not None)
+        remove_action.setEnabled(current_item is not None)
+        chosen_action = context_menu.exec_(event.globalPos())
+        if chosen_action == open_in_new_tab_action:
+            self.open_bookmarkInNewTab.emit(current_item.data(_url_role))
+        elif chosen_action == remove_action:
+            self._remove_item(current_item)
 
-    def _removeItem(self, item):
+    def _remove_item(self, item):
         button = QMessageBox.question(self, "Remove",
             "Would you like to remove \"{}\"?".format(item.text()),
             QMessageBox.Yes | QMessageBox.No)
         if button == QMessageBox.Yes:
             item.parent().removeRow(item.row())
 
-    def writeBookmarks(self):
+    def write_bookmarks(self):
         if not self._modified:
             return
-        dirPath = _configDir()
-        nativeDirPath = QDir.toNativeSeparators(dirPath)
-        dir = QFileInfo(dirPath)
+        dir_path = _config_dir()
+        native_dir_path = QDir.toNativeSeparators(dir_path)
+        dir = QFileInfo(dir_path)
         if not dir.isDir():
-            print('Creating {}...'.format(nativeDirPath))
+            print('Creating {}...'.format(native_dir_path))
             if not QDir(dir.absolutePath()).mkpath(dir.fileName()):
-                warnings.warn('Cannot create {}.'.format(nativeDirPath),
+                warnings.warn('Cannot create {}.'.format(native_dir_path),
                               RuntimeWarning)
                 return
-        serializedModel = _serializeModel(self._model, dirPath)
-        bookmarkFileName = os.path.join(nativeDirPath, _bookmarkFile)
-        print('Writing {}...'.format(bookmarkFileName))
-        with open(bookmarkFileName, 'w') as bookmarkFile:
-            json.dump(serializedModel, bookmarkFile, indent = 4)
+        serialized_model = _serialize_model(self._model, dir_path)
+        bookmark_file_name = os.path.join(native_dir_path, _bookmark_file)
+        print('Writing {}...'.format(bookmark_file_name))
+        with open(bookmark_file_name, 'w') as bookmark_file:
+            json.dump(serialized_model, bookmark_file, indent = 4)
 
-    def _readBookmarks(self):
-        bookmarkFileName = os.path.join(QDir.toNativeSeparators(_configDir()),
-                                        _bookmarkFile)
-        if os.path.exists(bookmarkFileName):
-            print('Reading {}...'.format(bookmarkFileName))
-            return json.load(open(bookmarkFileName))
-        return _defaultBookMarks
+    def _read_bookmarks(self):
+        bookmark_file_name = os.path.join(QDir.toNativeSeparators(_config_dir()),
+                                        _bookmark_file)
+        if os.path.exists(bookmark_file_name):
+            print('Reading {}...'.format(bookmark_file_name))
+            return json.load(open(bookmark_file_name))
+        return _default_bookmarks
 
     # Return a short title for a bookmark action,
     # "Qt | Cross Platform.." -> "Qt"
     @staticmethod
-    def shortTitle(t):
+    def short_title(t):
         i = t.find(' | ')
         if i == -1:
             i = t.find(' - ')

examples/webenginewidgets/tabbedbrowser/browsertabwidget.py
@@ -51,41 +51,41 @@ from PySide2.QtWebEngineWidgets import (QWebEngineDownloadItem,
 
 class BrowserTabWidget(QTabWidget):
 
-    urlChanged = QtCore.Signal(QUrl)
-    enabledChanged = QtCore.Signal(QWebEnginePage.WebAction, bool)
-    downloadRequested = QtCore.Signal(QWebEngineDownloadItem)
+    url_changed = QtCore.Signal(QUrl)
+    enabled_changed = QtCore.Signal(QWebEnginePage.WebAction, bool)
+    download_requested = QtCore.Signal(QWebEngineDownloadItem)
 
-    def __init__(self, windowFactoryFunction):
+    def __init__(self, window_factory_function):
         super(BrowserTabWidget, self).__init__()
         self.setTabsClosable(True)
-        self._windowFactoryFunction = windowFactoryFunction
+        self._window_factory_function = window_factory_function
         self._webengineviews = []
-        self.currentChanged.connect(self._currentChanged)
-        self.tabCloseRequested.connect(self.handleTabCloseRequest)
-        self._actionsEnabled = {}
-        for webAction in WebEngineView.webActions():
-            self._actionsEnabled[webAction] = False
+        self.currentChanged.connect(self._current_changed)
+        self.tabCloseRequested.connect(self.handle_tab_close_request)
+        self._actions_enabled = {}
+        for web_action in WebEngineView.web_actions():
+            self._actions_enabled[web_action] = False
 
-        tabBar = self.tabBar()
-        tabBar.setSelectionBehaviorOnRemove(QTabBar.SelectPreviousTab)
-        tabBar.setContextMenuPolicy(Qt.CustomContextMenu)
-        tabBar.customContextMenuRequested.connect(self._handleTabContextMenu)
+        tab_bar = self.tabBar()
+        tab_bar.setSelectionBehaviorOnRemove(QTabBar.SelectPreviousTab)
+        tab_bar.setContextMenuPolicy(Qt.CustomContextMenu)
+        tab_bar.customContextMenuRequested.connect(self._handle_tab_context_menu)
 
-    def addBrowserTab(self):
-        factoryFunc = partial(BrowserTabWidget.addBrowserTab, self)
-        webEngineView = WebEngineView(factoryFunc, self._windowFactoryFunction)
+    def add_browser_tab(self):
+        factory_func = partial(BrowserTabWidget.add_browser_tab, self)
+        web_engine_view = WebEngineView(factory_func, self._window_factory_function)
         index = self.count()
-        self._webengineviews.append(webEngineView)
+        self._webengineviews.append(web_engine_view)
         title = 'Tab {}'.format(index + 1)
-        self.addTab(webEngineView, title)
-        page = webEngineView.page()
-        page.titleChanged.connect(self._titleChanged)
-        page.iconChanged.connect(self._iconChanged)
-        page.profile().downloadRequested.connect(self._downloadRequested)
-        webEngineView.urlChanged.connect(self._urlChanged)
-        webEngineView.enabledChanged.connect(self._enabledChanged)
+        self.addTab(web_engine_view, title)
+        page = web_engine_view.page()
+        page.titleChanged.connect(self._title_changed)
+        page.iconChanged.connect(self._icon_changed)
+        page.profile().downloadRequested.connect(self._download_requested)
+        web_engine_view.urlChanged.connect(self._url_changed)
+        web_engine_view.enabled_changed.connect(self._enabled_changed)
         self.setCurrentIndex(index)
-        return webEngineView
+        return web_engine_view
 
     def load(self, url):
         index = self.currentIndex()
@@ -101,120 +101,120 @@ class BrowserTabWidget(QTabWidget):
         index = self.currentIndex()
         return self._webengineviews[index].url() if index >= 0 else QUrl()
 
-    def _urlChanged(self, url):
+    def _url_changed(self, url):
         index = self.currentIndex()
         if index >= 0 and self._webengineviews[index] == self.sender():
-                self.urlChanged.emit(url)
+                self.url_changed.emit(url)
 
-    def _titleChanged(self, title):
-        index = self._indexOfPage(self.sender())
+    def _title_changed(self, title):
+        index = self._index_of_page(self.sender())
         if (index >= 0):
-            self.setTabText(index, BookmarkWidget.shortTitle(title))
+            self.setTabText(index, BookmarkWidget.short_title(title))
 
-    def _iconChanged(self, icon):
-        index = self._indexOfPage(self.sender())
+    def _icon_changed(self, icon):
+        index = self._index_of_page(self.sender())
         if (index >= 0):
             self.setTabIcon(index, icon)
 
-    def _enabledChanged(self, webAction, enabled):
+    def _enabled_changed(self, web_action, enabled):
         index = self.currentIndex()
         if index >= 0 and self._webengineviews[index] == self.sender():
-            self._checkEmitEnabledChanged(webAction, enabled)
+            self._check_emit_enabled_changed(web_action, enabled)
 
-    def _checkEmitEnabledChanged(self, webAction, enabled):
-        if enabled != self._actionsEnabled[webAction]:
-            self._actionsEnabled[webAction] = enabled
-            self.enabledChanged.emit(webAction, enabled)
+    def _check_emit_enabled_changed(self, web_action, enabled):
+        if enabled != self._actions_enabled[web_action]:
+            self._actions_enabled[web_action] = enabled
+            self.enabled_changed.emit(web_action, enabled)
 
-    def _currentChanged(self, index):
-        self._updateActions(index)
-        self.urlChanged.emit(self.url())
+    def _current_changed(self, index):
+        self._update_actions(index)
+        self.url_changed.emit(self.url())
 
-    def _updateActions(self, index):
+    def _update_actions(self, index):
         if index >= 0 and index < len(self._webengineviews):
             view = self._webengineviews[index]
-            for webAction in WebEngineView.webActions():
-                enabled = view.isWebActionEnabled(webAction)
-                self._checkEmitEnabledChanged(webAction, enabled)
+            for web_action in WebEngineView.web_actions():
+                enabled = view.is_web_action_enabled(web_action)
+                self._check_emit_enabled_changed(web_action, enabled)
 
     def back(self):
-        self._triggerAction(QWebEnginePage.Back)
+        self._trigger_action(QWebEnginePage.Back)
 
     def forward(self):
-        self._triggerAction(QWebEnginePage.Forward)
+        self._trigger_action(QWebEnginePage.Forward)
 
     def reload(self):
-        self._triggerAction(QWebEnginePage.Reload)
+        self._trigger_action(QWebEnginePage.Reload)
 
     def undo(self):
-        self._triggerAction(QWebEnginePage.Undo)
+        self._trigger_action(QWebEnginePage.Undo)
 
     def redo(self):
-        self._triggerAction(QWebEnginePage.Redo)
+        self._trigger_action(QWebEnginePage.Redo)
 
     def cut(self):
-        self._triggerAction(QWebEnginePage.Cut)
+        self._trigger_action(QWebEnginePage.Cut)
 
     def copy(self):
-        self._triggerAction(QWebEnginePage.Copy)
+        self._trigger_action(QWebEnginePage.Copy)
 
     def paste(self):
-        self._triggerAction(QWebEnginePage.Paste)
+        self._trigger_action(QWebEnginePage.Paste)
 
-    def selectAll(self):
-        self._triggerAction(QWebEnginePage.SelectAll)
+    def select_all(self):
+        self._trigger_action(QWebEnginePage.SelectAll)
 
-    def zoomFactor(self):
+    def zoom_factor(self):
         return self._webengineviews[0].zoomFactor() if self._webengineviews else 1.0
 
-    def setZoomFactor(self, z):
+    def set_zoom_factor(self, z):
         for w in self._webengineviews:
             w.setZoomFactor(z)
 
-    def _handleTabContextMenu(self, point):
+    def _handle_tab_context_menu(self, point):
         index = self.tabBar().tabAt(point)
         if index < 0:
             return
-        tabCount = len(self._webengineviews)
-        contextMenu = QMenu()
-        duplicateTabAction = contextMenu.addAction("Duplicate Tab")
-        closeOtherTabsAction = contextMenu.addAction("Close Other Tabs")
-        closeOtherTabsAction.setEnabled(tabCount > 1)
-        closeTabsToTheRightAction = contextMenu.addAction("Close Tabs to the Right")
-        closeTabsToTheRightAction.setEnabled(index < tabCount - 1)
-        closeTabAction = contextMenu.addAction("&Close Tab")
-        chosenAction = contextMenu.exec_(self.tabBar().mapToGlobal(point))
-        if chosenAction == duplicateTabAction:
-            currentUrl = self.url()
-            self.addBrowserTab().load(currentUrl)
-        elif chosenAction == closeOtherTabsAction:
-            for t in range(tabCount - 1, -1, -1):
+        tab_count = len(self._webengineviews)
+        context_menu = QMenu()
+        duplicate_tab_action = context_menu.addAction("Duplicate Tab")
+        close_other_tabs_action = context_menu.addAction("Close Other Tabs")
+        close_other_tabs_action.setEnabled(tab_count > 1)
+        close_tabs_to_the_right_action = context_menu.addAction("Close Tabs to the Right")
+        close_tabs_to_the_right_action.setEnabled(index < tab_count - 1)
+        close_tab_action = context_menu.addAction("&Close Tab")
+        chosen_action = context_menu.exec_(self.tabBar().mapToGlobal(point))
+        if chosen_action == duplicate_tab_action:
+            current_url = self.url()
+            self.add_browser_tab().load(current_url)
+        elif chosen_action == close_other_tabs_action:
+            for t in range(tab_count - 1, -1, -1):
                 if t != index:
-                     self.handleTabCloseRequest(t)
-        elif chosenAction == closeTabsToTheRightAction:
-            for t in range(tabCount - 1, index, -1):
-                self.handleTabCloseRequest(t)
-        elif chosenAction == closeTabAction:
-            self.handleTabCloseRequest(index)
+                     self.handle_tab_close_request(t)
+        elif chosen_action == close_tabs_to_the_right_action:
+            for t in range(tab_count - 1, index, -1):
+                self.handle_tab_close_request(t)
+        elif chosen_action == close_tab_action:
+            self.handle_tab_close_request(index)
 
-    def handleTabCloseRequest(self, index):
+    def handle_tab_close_request(self, index):
         if (index >= 0 and self.count() > 1):
             self._webengineviews.remove(self._webengineviews[index])
             self.removeTab(index)
 
-    def closeCurrentTab(self):
-        self.handleTabCloseRequest(self.currentIndex())
+    def close_current_tab(self):
+        self.handle_tab_close_request(self.currentIndex())
 
-    def _triggerAction(self, action):
+    def _trigger_action(self, action):
         index = self.currentIndex()
         if index >= 0:
             self._webengineviews[index].page().triggerAction(action)
 
-    def _indexOfPage(self, webPage):
+    def _index_of_page(self, web_page):
         for p in range(0, len(self._webengineviews)):
-            if (self._webengineviews[p].page() == webPage):
+            if (self._webengineviews[p].page() == web_page):
                 return p
         return -1
 
-    def _downloadRequested(self, item):
+    def _download_requested(self, item):
         self.downloadRequested.emit(item)

commit 1e8896f509bd3435b97b6036c02fb177e5f5e526

TabbedBrowser Example: Fix "Open in new tab"
Restore override of QWebEngineView::createWindow()
broken by 90c1c767095e583d0315e87c0592597020858246.

Change-Id: I6cdc05c3b8edbfb9da24ca3d6502cec2ccce870a
Fixes: PYSIDE-1022
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

examples/webenginewidgets/tabbedbrowser/webengineview.py
@@ -79,7 +79,7 @@ class WebEngineView(QWebEngineView):
     def is_web_action_enabled(self, web_action):
         return self.page().action(web_action).isEnabled()
 
-    def create_window(self, window_type):
+    def createWindow(self, window_type):
         if window_type == QWebEnginePage.WebBrowserTab or window_type == QWebEnginePage.WebBrowserBackgroundTab:
             return self._tab_factory_func()
         return self._window_factory_func()