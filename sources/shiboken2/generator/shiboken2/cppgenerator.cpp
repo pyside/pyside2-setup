70102765aaeaa926bd65f4e2a192268420b85459
Friedemann Kleint <Friedemann.Kleint@qt.io>
2020-05-13 14:47:39 +0200

    Revert "Revert "Fix deprecation warnings about Qt::SplitBehavior""
    Fix warnings in 5.15.
    
    This reverts commit 2ca788f85fbc9d58969533d13046bbe19f53fac8.
    
    Change-Id: Iaee39e2035f5e4ebdc9b5360a68a5f024f8bde38
    Reviewed-by: Christian Tismer <tismer@stackless.com>

--- sources/shiboken2/generator/shiboken2/cppgenerator.cpp
+++ sources/shiboken2/generator/shiboken2/cppgenerator.cpp
@@ -1525,10 +1525,10 @@ void CppGenerator::writeConverterRegister(QTextStream &s, const AbstractMetaClas
     QStringList cppSignature;
     if (!classContext.forSmartPointer()) {
         cppSignature = metaClass->qualifiedCppName().split(QLatin1String("::"),
-                                                                       QString::SkipEmptyParts);
+                                                                       Qt::SkipEmptyParts);
     } else {
         cppSignature = classContext.preciseType()->cppSignature().split(QLatin1String("::"),
-                                                                        QString::SkipEmptyParts);
+                                                                        Qt::SkipEmptyParts);
     }
     while (!cppSignature.isEmpty()) {
         QString signature = cppSignature.join(QLatin1String("::"));
@@ -1976,7 +1976,7 @@ void CppGenerator::writeArgumentsInitializer(QTextStream &s, OverloadData &overl
 
     s << INDENT << "PyObject *";
     s << PYTHON_ARGS << "[] = {"
-        << QString(maxArgs, QLatin1Char('0')).split(QLatin1String(""), QString::SkipEmptyParts).join(QLatin1String(", "))
+        << QString(maxArgs, QLatin1Char('0')).split(QLatin1String(""), Qt::SkipEmptyParts).join(QLatin1String(", "))
         << "};\n";
     s << Qt::endl;
 
@@ -5876,7 +5876,7 @@ bool CppGenerator::finishGeneration()
         if (!referencedType)
             continue;
         QString converter = converterObject(referencedType);
-        QStringList cppSignature = pte->qualifiedCppName().split(QLatin1String("::"), QString::SkipEmptyParts);
+        QStringList cppSignature = pte->qualifiedCppName().split(QLatin1String("::"), Qt::SkipEmptyParts);
         while (!cppSignature.isEmpty()) {
             QString signature = cppSignature.join(QLatin1String("::"));
             s << INDENT << "Shiboken::Conversions::registerConverterName(" << converter << ", \"" << signature << "\");\n";
