1044476b0aa76a1f730b0d4bf452d46e60c6f4e8
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-05-15 14:32:53 +0200

    Add example contributions from Hacktoberfest 2019
    Adding examples contributed from Github.
    
    Thanks to the contributors:
    - María José Molina-Contreras https://github.com/mjmolina
    - Sabine Wolf https://github.com/Lythi45
    - Meili Triantafyllidi https://github.com/mei-li
    - Elena Hirjoaba https://github.com/puskini33
    
    Change-Id: I7abfc6149af2e02c947ab9f088af1d09492de5df
    Reviewed-by: Christian Tismer <tismer@stackless.com>

---
+++ examples/charts/piechart.py
@@ -0,0 +1,87 @@
+#############################################################################
+##
+## Copyright (C) 2020 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the Qt for Python examples of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:BSD$
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##   * Neither the name of The Qt Company Ltd nor the names of its
+##     contributors may be used to endorse or promote products derived
+##     from this software without specific prior written permission.
+##
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+"""PySide2 port of the Pie Chart Example from Qt v5.x"""
+
+import sys
+from PySide2.QtCore import Qt
+from PySide2.QtGui import QPainter, QPen
+from PySide2.QtWidgets import QMainWindow, QApplication
+from PySide2.QtCharts import QtCharts
+
+
+class TestChart(QMainWindow):
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.series = QtCharts.QPieSeries()
+
+        self.series.append('Jane', 1)
+        self.series.append('Joe', 2)
+        self.series.append('Andy', 3)
+        self.series.append('Barbara', 4)
+        self.series.append('Axel', 5)
+
+        self.slice = self.series.slices()[1]
+        self.slice.setExploded()
+        self.slice.setLabelVisible()
+        self.slice.setPen(QPen(Qt.darkGreen, 2))
+        self.slice.setBrush(Qt.green)
+
+        self.chart = QtCharts.QChart()
+        self.chart.addSeries(self.series)
+        self.chart.setTitle('Simple piechart example')
+        self.chart.legend().hide()
+
+        self.chartView = QtCharts.QChartView(self.chart)
+        self.chartView.setRenderHint(QPainter.Antialiasing)
+
+        self.setCentralWidget(self.chartView)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = TestChart()
+    window.show()
+    window.resize(440, 300)
+
+    sys.exit(app.exec_())
