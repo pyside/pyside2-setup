f2e63d3588c8422d84d9ff5480427a83d68083bb
Friedemann Kleint <Friedemann.Kleint@qt.io>
2020-06-11 07:56:19 +0200

    shiboken: Fix default-initialized function arguments
    Specifying {} causes it to be qualified, check this.
    
    Change-Id: Idd23c8a5af01cd7fbb63a2e5a01bb349c530fe54
    Fixes: PYSIDE-1325
    Reviewed-by: Christian Tismer <tismer@stackless.com>

--- sources/shiboken2/tests/libsample/pen.h
+++ sources/shiboken2/tests/libsample/pen.h
@@ -49,10 +49,15 @@ class LIBSAMPLE_API Pen
 public:
     enum { EmptyCtor, EnumCtor, ColorCtor, CopyCtor };
 
+    enum  RenderHints { None = 0, Antialiasing = 0x1, TextAntialiasing = 0x2 };
+
     Pen();
     Pen(SampleNamespace::Option option);
     Pen(const Color& color);
     Pen(const Pen& pen);
+
+    // PYSIDE-1325, default initializer
+    void drawLine(int x1, int y1, int x2, int y2, RenderHints renderHints = {});
 
     int ctorType();
 private:
