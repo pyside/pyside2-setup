d6f460db0f9fcdc2756b29209f21a7b87587881c
Kyle Altendorf <sda@fstab.net>
2020-06-12 22:46:49 -0400

    basewrapper.cpp: add PyErr_Fetch/Restore in SbkDeallocWrapperCommon()
    Fixes corruption of the interpreter's internal exception state which can
    occur for example in a final send to a generator which results in
    teardown of Python objects.
    
    Resolved by adding PyErr_Fetch and PyErr_Restore calls around the
    deallocation code in SbkDeallocWrapperCommon().
    
    Task-number: PYSIDE-1313
    Change-Id: Ie68c7b08d60eec8ca91b4032b21089d66d6e4478
    Reviewed-by: Christian Tismer <tismer@stackless.com>

---
+++ sources/pyside2/tests/QtCore/bug_1313.py
@@ -0,0 +1,85 @@
+#############################################################################
+##
+## Copyright (C) 2020 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the test suite of Qt for Python.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+''' unit test for BUG #1313 '''
+
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from init_paths import init_test_paths
+init_test_paths(False)
+
+from PySide2 import QtCore
+import py3kcompat as py3k
+
+
+class MyQObject(QtCore.QObject):
+    sig = QtCore.Signal()
+
+
+demo_coroutine_definition_code = """
+async def demo_coroutine():
+    my_qobject = MyQObject()
+    my_qobject.sig.connect(lambda: None)
+"""
+
+
+if py3k.IS_PY3K:
+    exec(demo_coroutine_definition_code)
+
+
+@unittest.skipIf(not py3k.IS_PY3K, "Requires Python 3 due to use of async def")
+class CoroutineRaisesStopIterationTestCase(unittest.TestCase):
+    def setUp(self):
+        self.coroutine = demo_coroutine()
+
+    def testCoroutine(self):
+        with self.assertRaises(StopIteration):
+            self.coroutine.send(None)
+
+
+def demo_generator():
+    my_qobject = MyQObject()
+    my_qobject.sig.connect(lambda: None)
+    return
+    yield # to make it a generator
+
+
+class GeneratorRaisesStopIterationTestCase(unittest.TestCase):
+    def setUp(self):
+        self.generator = demo_generator()
+
+    def testGenerator(self):
+        with self.assertRaises(StopIteration):
+            self.generator.send(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
